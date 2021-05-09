41b44d00e1c2ace1de8170f2188e61fadfc59c42
Amitav Roy <reachme@amitavroy.com>
2021-05-09 23:28:24 +0530

    added the tests from the live video

--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
-class UserRegistrationTest extends TestCase
+class RegistrationTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
@@ -22,16 +22,15 @@ class UserRegistrationTest extends TestCase
     }
 
     /** @test */
-    public function it_registers_a_user_with_verified_at_as_null()
+    public function it_registers_a_user_with_verified_as_null()
     {
         $postData = $this->userRegistrationBaseData();
 
         $this->json('POST', route('user.register'), $postData);
 
-
         $this->assertDatabaseHas('users', [
             'email' => $postData['email'],
-            'email_verified_at' => null,
+            'email_verified_at' => null
         ]);
     }
 
@@ -39,38 +38,52 @@ class UserRegistrationTest extends TestCase
     public function it_validates_required_fields()
     {
         $this->json('POST', route('user.register'), [])
+            ->assertStatus(422)
             ->assertJson(function (AssertableJson $json) {
-                $json->has('errors')
-                    ->has('errors.email')
-                    ->has('errors.name')
+                $json->has('errors.email')
                     ->has('errors.password')
                     ->has('errors.confirm')
+                    ->has('errors.name')
                     ->etc();
             });
     }
 
     /** @test */
-    public function it_requires_unique_email()
+    public function it_validates_passwords_are_same()
+    {
+        $postData = $this->userRegistrationBaseData();
+        $postData['confirm'] = 'cpassword';
+
+        $this->json('POST', route('user.register'), $postData)
+            ->assertStatus(422)
+            ->assertJson(function (AssertableJson $json) {
+                $json->has('errors.confirm')
+                    ->etc();
+            });
+    }
+
+    /** @test */
+    public function it_validates_unique_email()
     {
         $user = User::factory()->create();
+        $postData = $this->userRegistrationBaseData();
+        $postData['email'] = $user->email;
 
-        $defaultUser = $this->userRegistrationBaseData();
-        $defaultUser['email'] = $user->email;
-
-        $this->json('POST', route('user.register'), $defaultUser)
+        $this->json('POST', route('user.register'), $postData)
+            ->assertStatus(422)
             ->assertJson(function (AssertableJson $json) {
                 $json->has('errors.email')
                     ->etc();
             });
     }
 
-    private function userRegistrationBaseData(): array
+    public function userRegistrationBaseData(): array
     {
         return [
             'email' => $this->faker->email,
+            'name' => $this->faker->name(),
             'password' => 'password',
             'confirm' => 'password',
-            'name' => $this->faker->name(),
         ];
     }
 }
